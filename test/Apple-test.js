85ed4b1f7b9fb8a713c8764b7e6c969951e3b36a
Adam Mescher <admescher@gmail.com>
2017-08-21 13:56:09 -0600

    Add test files for Apple and Snake classes

--- test/Apple-test.js
+++ test/Apple-test.js
@@ -2,33 +2,30 @@ const {assert, expect, should} = require('chai');
 const Apple = require('../lib/apple');
 
 describe('Apple', function() {
+
+  let apple;
+
+  beforeEach( function() {
+    apple = new Apple();
+  });
+
   it('should be a function', function() {
     assert.isFunction(Apple);
   });
-  it('should be an object', function(){
-    const apple = new Apple();
 
+  it('should be an object', function(){
     assert.isObject(apple);
   });
-  it('should have a color', function() {
-    const apple = new Apple();
 
-    assert.equal( apple.color, 'rgb(213, 16, 16)' );
-  });
   it('should have an x and y position', function() {
-    const apple = new Apple();
-
     assert.equal( typeof(apple.xPosition), 'number' );
     assert.equal( typeof(apple.yPosition), 'number' );
   });
+
   it('should generate a new x and y position between 0 and 29', function() {
-    const apple = new Apple();
     apple.generateRandomLocation();
 
     assert.isAbove( apple.xPosition, -1 );
     assert.isBelow( apple.yPosition, 31 );
   });
-  it.skip('should be able to draw itself', function() {
-
-  })
-})
+});
--- test/Snake-test.js
+++ test/Snake-test.js
@@ -1,118 +1,101 @@
 const {assert, expect, should} = require('chai');
 const Snake = require('../lib/snake');
 
+let snake;
+
+beforeEach(function() {
+  snake = new Snake();
+});
+
 describe('Snake', function() {
   it( 'should be a function', function(){
-    const snake = new Snake();
-
     assert.isFunction( Snake );
   });
-  it(' should be an object', function(){
-    const snake = new Snake();
 
+  it(' should be an object', function(){
     assert.isObject( snake );
   });
-  it(' should have a color', function() {
-    const snake = new Snake();
 
-    assert.equal( snake.color, 'rgb(16, 213, 16)' );
-  });
   it(' should have a tail that is 4 units long', function() {
-    const snake = new Snake();
     assert.equal( snake.tail, 4 );
   });
-  it(' should have an x and y position', function() {
-    const snake = new Snake();
 
+  it(' should have an x and y position', function() {
     assert.equal( typeof(snake.xPosition), 'number' );
     assert.equal( typeof(snake.xPosition), 'number' );
   });
-  it(' should have an x and y velocity', function() {
-    const snake = new Snake();
 
+  it(' should have an x and y velocity', function() {
     assert.equal( typeof(snake.xVelocity), 'number' );
     assert.equal( typeof(snake.yVelocity), 'number' );
   });
-  it(' should have a trail', function() {
-    const snake = new Snake();
 
+  it(' should have a trail', function() {
     assert.isArray(snake.trail)
   });
+
   it.skip('should be able to change direction', function() {
-    const snake = new Snake();
+
   });
+
   it(' should be able to move up', function() {
-    const snake = new Snake();
-
     snake.moveUp();
 
     assert.equal( snake.xVelocity, 0 );
     assert.equal( snake.yVelocity, -1 );
   });
+
   it(' should be able to move left', function() {
-    const snake = new Snake();
-
     snake.moveUp();
     snake.moveLeft();
 
     assert.equal( snake.xVelocity, -1 );
     assert.equal( snake.yVelocity, 0 );
   });
+
   it(' should be able to move down', function() {
-    const snake = new Snake();
-
     snake.moveDown();
 
     assert.equal( snake.xVelocity, 0 );
     assert.equal( snake.yVelocity, 1 );
   });
-  it(' should be able to move right', function() {
-    const snake = new Snake();
 
+  it(' should be able to move right', function() {
     snake.moveRight();
 
     assert.equal( snake.xVelocity, 1 );
     assert.equal( snake.yVelocity, 0 );
   });
-  it(' should not be able to move left if moving right', function() {
-    const snake = new Snake();
 
+  it(' should not be able to move left if moving right', function() {
     snake.moveLeft();
 
     assert.equal( snake.xVelocity, 1 );
     assert.equal( snake.yVelocity, 0 );
   });
-  it(' should not be able to move right if moving left', function() {
-    const snake = new Snake();
 
+  it(' should not be able to move right if moving left', function() {
     snake.moveUp();
     snake.moveLeft();
     snake.moveRight();
 
     assert.equal( snake.xVelocity, -1 );
     assert.equal( snake.yVelocity, 0 );
-  })
-  it(' should not be able to move up if moving down', function() {
-    const snake = new Snake();
+  });
 
+  it(' should not be able to move up if moving down', function() {
     snake.moveDown();
     snake.moveUp();
 
     assert.equal( snake.xVelocity, 0 );
     assert.equal( snake.yVelocity, 1 );
   });
-  it(' should not be able to move down if moving up', function() {
-    const snake = new Snake();
 
+  it(' should not be able to move down if moving up', function() {
     snake.moveUp();
     snake.moveDown();
 
     assert.equal( snake.xVelocity, 0 );
     assert.equal( snake.yVelocity, -1 );
   });
-  it.skip('should be able to draw itself', function() {
-    const snake = new Snake();
-    snake.draw();
-  });
-
 });
